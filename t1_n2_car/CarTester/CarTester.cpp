2fc51cf3129b1b6e93e08f6800eaf101eca9d33d
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-03-27 23:20:11 +0300

    t1_n2_car: various tests added
    No instant direction change allowed
    Engine can be shut down only when car is still and gear is neutral
    Car reports about repetitive attempts to turn engine on or off
    Setting gear to the same mode is successful

--- t1_n2_car/CarTester/CarTester.cpp
+++ t1_n2_car/CarTester/CarTester.cpp
@@ -93,4 +93,63 @@ BOOST_AUTO_TEST_CASE(NeutralGearSlowdownOnly)
 	BOOST_CHECK(car.SetSpeed(29) == CCar::SetSpeedResult::SUCCESS);
 }
 
+BOOST_AUTO_TEST_CASE(NoReverseWhileMovingForward)
+{
+	car.TurnOnEngine();
+	car.SetGear(1);
+	car.SetSpeed(1);
+	BOOST_CHECK(car.SetGear(-1) == CCar::SetGearResult::UNSUITABLE_DIRECTION);
+}
+
+BOOST_AUTO_TEST_CASE(NoForwardGearWhileMovingBackward)
+{
+	car.TurnOnEngine();
+	car.SetGear(-1);
+	car.SetSpeed(1);
+	BOOST_CHECK(car.SetGear(0) == CCar::SetGearResult::SUCCESS);
+	BOOST_CHECK(car.SetGear(1) == CCar::SetGearResult::UNSUITABLE_DIRECTION);
+}
+
+BOOST_AUTO_TEST_CASE(EngineCanBeTurnedOffOnlyIfCarStillAndGearNeutral)
+{
+	car.TurnOnEngine();
+
+	car.SetGear(1);
+	BOOST_CHECK(!car.TurnOffEngine());
+	BOOST_CHECK(car.EngineTurnedOn());
+
+	car.SetSpeed(1);
+	BOOST_CHECK(!car.TurnOffEngine());
+	BOOST_CHECK(car.EngineTurnedOn());
+
+	car.SetGear(0);
+	BOOST_CHECK(!car.TurnOffEngine());
+	BOOST_CHECK(car.EngineTurnedOn());
+
+	car.SetSpeed(0);
+	BOOST_CHECK(car.TurnOffEngine());
+	BOOST_CHECK(!car.EngineTurnedOn());
+}
+
+BOOST_AUTO_TEST_CASE(TurnEngineOnTwice)
+{
+	car.TurnOnEngine();
+	BOOST_CHECK(!car.TurnOnEngine());
+	BOOST_CHECK(car.EngineTurnedOn());
+}
+
+BOOST_AUTO_TEST_CASE(TurnEngineOffTwice)
+{
+	car.TurnOffEngine();
+	BOOST_CHECK(!car.TurnOffEngine());
+	BOOST_CHECK(!car.EngineTurnedOn());
+}
+
+BOOST_AUTO_TEST_CASE(SetGearToSameMode)
+{
+	car.TurnOnEngine();
+	car.SetGear(-1);
+	BOOST_CHECK(car.SetGear(-1) == CCar::SetGearResult::SUCCESS);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
